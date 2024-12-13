af0e5fe9d32e041a2cdcde2024c638b4a1f86686
IlyaGlaz <glazterix@gmail.com>
2024-12-13 23:56:59 +0300

    init

---
+++ src/main/java/Logic.java
@@ -0,0 +1,12 @@
+public class Logic {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        int number = 20; // вынести эти две строчки в метод и вызвать этот метод в этом месте
+        int result = number * number; // вынести эти две строчки в метод и вызвать этот метод в этом месте
+
+        System.out.println(result); // эту строчку оставить здесь, чтобы рапечатывался результат, который вернется из метода, который ты напишешь
+    }
+}
